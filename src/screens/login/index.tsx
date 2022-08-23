93b65eada334c1b944fed7df880a1e6f70cd4720
CarlosWGama <carloswgama@gmail.com>
2022-08-23 15:48:11 -0300

    State

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,52 +1,66 @@
+
 import { View, Text, ImageBackground, StyleSheet, Alert } from 'react-native';
 import bg from './../../assets/imgs/bg.png';
 import { Input } from "@rneui/themed";
 import { Button } from '@rneui/base';
+import { useState } from 'react';
 
 export interface LoginScreenProps {
 }
 
 export function LoginScreen(props: LoginScreenProps) {
 
+    const [ email, setEmail ] = useState('');
+    const [ senha, setSenha ] = useState('');
 
+    const logar = () => {
+      if (email.trim() == 'teste@teste.com' && senha == '123456')
+        console.log('Logado com sucesso');
+      else
+        console.log('Email ou senha incorreta!');
+    }
 
-      return (
-          <ImageBackground source={bg} style={styles.background}>
-              <View style={styles.container}>
-                  <Text style={styles.logo}>APP</Text>
-                  
-                  {/* EMAIL */ }
-                  <Input 
-                    placeholder='Digite seu email' 
-                    inputContainerStyle={styles.inputContainer}
-                    inputStyle={{color:'white'}}
-                    placeholderTextColor="lightgrey"
-                    leftIcon={{name:'person', color:'white'}}
+    return (
+        <ImageBackground source={bg} style={styles.background}>
+            <View style={styles.container}>
+                <Text style={styles.logo}>APP</Text>
+                
+                {/* EMAIL */ }
+                <Input 
+                  placeholder='Digite seu email' 
+                  inputContainerStyle={styles.inputContainer}
+                  inputStyle={{color:'white'}}
+                  value={email}
+                  onChangeText={setEmail}
+                  placeholderTextColor="lightgrey"
+                  leftIcon={{name:'person', color:'white'}}
+                />
+                
+                {/* SENHA */}
+                <Input 
+                  placeholder='Digite sua senha'
+                  inputContainerStyle={styles.inputContainer}
+                  placeholderTextColor="lightgrey" 
+                  inputStyle={{color:'white'}}
+                  value={senha}
+                  onChangeText={setSenha}
+                  leftIcon={{name:'lock', color:'white'}}
+                  secureTextEntry={true}
                   />
-                  
-                  {/* SENHA */}
-                  <Input 
-                    placeholder='Digite sua senha'
-                    inputContainerStyle={styles.inputContainer}
-                    placeholderTextColor="lightgrey" 
-                    inputStyle={{color:'white'}}
-                    leftIcon={{name:'lock', color:'white'}}
-                    secureTextEntry={true}
-                    />
 
-                    {/* BOTÃO */}
-                    <Button title='Logar' 
-                        buttonStyle={styles.btn} 
-          
-                    />
+                  {/* BOTÃO */}
+                  <Button title='Logar' 
+                      buttonStyle={styles.btn} 
+                      onPress={logar}
+                  />
 
-                    {/* OPÇÃO CADASTRAR */}
-                    <Text style={styles.cadastrar}>Não possui conta?
-                      {'\n'}
-                     Clique aqui para se cadastrar</Text>
-              </View>
-          </ImageBackground>
-      );
+                  {/* OPÇÃO CADASTRAR */}
+                  <Text style={styles.cadastrar}>Não possui conta?
+                    {'\n'}
+                    Clique aqui para se cadastrar</Text>
+            </View>
+        </ImageBackground>
+    );
   }
 
   const styles = StyleSheet.create({
