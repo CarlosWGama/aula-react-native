86b83179e689eee0cd9d139a22bfefc8cabed602
CarlosWGama <carloswgama@gmail.com>
2022-08-29 21:15:51 -0300

    Formulário

--- src/screens/login/input.tsx
+++ src/screens/login/input.tsx
@@ -5,26 +5,27 @@ import { Input } from '@rneui/base';
 export interface InputRoundProps {
     texto?:string;
     senha?:boolean;
+    onBlur?(campo:any):void;
     icone: string;
     placeholder: string;
     onChangeText?(text:string):void;
 }
 
+
 function _InputRound (props: InputRoundProps) {
-    
     return (
-      <View>
-         { props.texto && <Text style={styles.texto}>{props.texto}</Text>}
-         <Input 
-                  placeholder={props.placeholder}
-                  inputContainerStyle={styles.inputContainer}
-                  inputStyle={{color:'white'}}
-                  onChangeText={props.onChangeText}
-                  placeholderTextColor="lightgrey"
-                  secureTextEntry={props.senha}
-                  leftIcon={{name:props.icone, color:'white'}}
-                />
-      </View>
+        <View>
+            {props.texto && <Text style={styles.texto}>{props.texto}</Text>}
+            <Input placeholder={props.placeholder}  
+            leftIcon={{name:props.icone, color:'white'}}
+            onBlur={props.onBlur}
+            placeholderTextColor="white"
+            inputContainerStyle={styles.inputContainer}
+            inputStyle={{color:'white'}} 
+            onChangeText={props.onChangeText}
+            secureTextEntry={props.senha}
+            />
+        </View>
     );
 }
 
